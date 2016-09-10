0aa50299e06ba8fe445228085a3178788849a269
Bradley Lowekamp <blowekamp@mail.nih.gov>
2016-09-10 00:12:12 -0400

    Adding appveyor CI support (#5)
    * Add initial yml configuration for appveyor
    
    Separate the initial checkout and the update of the Cached ITK
    repository into a bash script to enable reuse in different ci
    integration configurations.
    
    * Add common ctest script for ci builds
    
    * Remove overrride in function definition

---
+++ test/ci/CacheAndUpdateITK.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+env
+
+mkdir -p "${ExternalData_OBJECT_STORES}"
+
+( cd ${ExternalData_OBJECT_STORES} &&
+    if [[ ! -e ${ITK_REPOSITORY} ]]
+    then
+        git clone --mirror ${ITK_REPOSITORY_REMOTE}
+    fi &&
+    cd ${ITK_REPOSITORY} &&
+    git remote update
+)
+
+git clone --single-branch ${ITK_REPOSITORY} -b "${ITK_TAG}" "${ITK_SRC}" &&
+ ( cd ${ITK_SRC}/Modules/Remote &&
+     ln -s ${PROJ_SRC} ${ITK_MODULE_NAME}
+ )
+
+( cd ${PROJ_SRC}/test &&
+    git clone --single-branch ${ITK_REPOSITORY} -b dashboard dashboard
+)
